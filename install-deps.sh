242f20ce8c0a32331b3fdd4a0771626b171e27c1
Marius Milcher <mmilcher@Mariuss-Mac.local>
2014-04-01 17:02:15 +0100

    Further edits

--- install-deps.sh
+++ install-deps.sh
@@ -7,12 +7,12 @@ bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # https://rvm.io
 # rvm for the rubiess
-curl -L https://get.rvm.io | bash -s stable --ruby
+# curl -L https://get.rvm.io | bash -s stable --ruby
 
 # https://github.com/isaacs/nave
 # needs npm, obviously.
 # TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
-npm install -g nave
+# npm install -g nave
 
 # homebrew!
 # you need the code CLI tools YOU FOOL.
@@ -20,7 +20,7 @@ ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
 # https://github.com/rupa/z
 # z, oh how i love you
-cd ~/code
+mkdir ~/code && cd ~/code
 git clone https://github.com/rupa/z.git
 chmod +x ~/code/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
@@ -29,13 +29,13 @@ chmod +x ~/code/z/z.sh
 
 # https://github.com/dronir/SpotifyControl
 # Spotify Controll Script
-cd ~/code
-git clone git://github.com/dronir/SpotifyControl.git
+#cd ~/code
+#git clone git://github.com/dronir/SpotifyControl.git
 
 # https://github.com/jeroenbegyn/VLCControl
 # VLC Controll Script
-cd ~/code
-git clone git://github.com/jeroenbegyn/VLCControl.git
+#cd ~/code
+#git clone git://github.com/jeroenbegyn/VLCControl.git
 
 
 # my magic photobooth symlink -> dropbox. I love it.
@@ -48,7 +48,7 @@ git clone git://github.com/jeroenbegyn/VLCControl.git
 
 
 # for the c alias (syntax highlighted cat)
-sudo easy_install Pygments
+# sudo easy_install Pygments
 
 
 # chrome canary as default
